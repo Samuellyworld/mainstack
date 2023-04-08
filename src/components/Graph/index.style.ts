f5c417c04e3ab8900621ef6ab1687347307d7f28
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-04-08 01:50:56 +0100

    feat : setting dynamic graph UI ⌛️

---
+++ src/components/Graph/index.style.ts
@@ -0,0 +1,19 @@
+// import styled from styled-components
+import styled from "styled-components";
+
+
+// graph container styles
+export const GraphContainer = styled.div`
+  height: 40vh;
+  margin-bottom: 2rem;
+  width: 100%;
+  z-index: 6000;
+
+  .recharts-cartesian-axis-tick-value {
+    font-size: 0.8rem;
+  }
+
+  .recharts-tooltip-label {
+    font-size: 0.8rem;
+  }
+`;
